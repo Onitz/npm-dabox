382bdf7a649b97b3f034e4210212d32b6391060f
Dabson <cyphlix@gmail.com>
2018-08-04 13:30:41 +1000

    add getUrl function

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 exports.getVersion = function() {
   console.log('Dabox version 0.0.1');
 }
+
+function getUrl( url, callback ) {
+  let xhr = new XMLHttpRequest();
+  xhr.open( "GET", url, true );
+  xhr.responseType = "json";
+  xhr.onload = function() {
+    let status = xhr.status;
+    if (status === 200) {
+      callback( null, xhr.response );
+    } else {
+      callback( status, xhr.response );
+    }
+  };
+  xhr.send();
+}
