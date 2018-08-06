7d583f426c3e9053ec1165b624cf27ac79d3628b
Dabson <cyphlix@gmail.com>
2018-08-06 15:02:10 +1000

    improve listener function, add initCanvas

--- index.js
+++ index.js
@@ -1,76 +1,92 @@
-let packageJson = require('./package.json');
+let packageJson = require('./package.json')
 
 exports.getVersion = function() {
-  return packageJson.name + '@' + packageJson.version;
+  return packageJson.name + '@' + packageJson.version
 }
 
 exports.getUrl = function( url, callback ) {
-  let xhr = new XMLHttpRequest();
-  xhr.open( "GET", url, true );
-  xhr.responseType = "json";
+  let xhr = new XMLHttpRequest()
+  xhr.open( "GET", url, true )
+  xhr.responseType = "json"
   xhr.onload = function() {
-    let status = xhr.status;
+    let status = xhr.status
     if (status === 200) {
-      callback( null, xhr.response );
+      callback( null, xhr.response )
     } else {
-      callback( status, xhr.response );
+      callback( status, xhr.response )
     }
-  };
-  xhr.send();
-};
+  }
+  xhr.send()
+}
 
-exports.keyListen = function( isEnable, up, down, left, right, space, ctrl) {
+/*
+ * All arguments are callbacks, isEnable, up, down, left, right are mandatory
+ */
+exports.keyListen = function( isEnable, up, down, left, right, space, ctrl, esc, tab ) {
   window.onkeydown = function( e ) {
     if( !isEnable() ) {
-      return;
+      return
     }
-    var key = e.which;
-    var keyLeft = 37;
-    var keyUp = 38;
-    var keyRight = 39;
-    var keyDown = 40;
-    var keySpace = 32;
-    var keyW = 87;
-    var keyA = 65;
-    var keyS = 83;
-    var keyD = 68;
+    let key = e.which
+    let keyLeft = 37
+    let keyUp = 38
+    let keyRight = 39
+    let keyDown = 40
+    let keySpace = 32
+    let keyW = 87
+    let keyA = 65
+    let keyS = 83
+    let keyD = 68
+    let keyEsc = 27
+    let keyTab = 9
 
     if( key === keyUp || key === keyW ) {
-      up();
-    } else if( key === keyDown || key === keyS ) {
-      down();
+      up()
     } else if( key === keyLeft || key === keyA ) {
-      left();
+      left()
+    } else if( key === keyDown || key === keyS ) {
+      down()
     } else if( key === keyRight || key === keyD ) {
-      right();
-    } else if( key === keySpace ) {
-      space();
-    } else if( e.ctrlKey ) {
-      ctrl();
+      right()
+    } else if( space && key === keySpace ) {
+      space()
+    } else if( ctrl && e.ctrlKey ) {
+      ctrl()
+    } else if( esc && key === keyEsc ) {
+      esc()
+    } else if( tab && key === keyTab ) {
+      e.preventDefault()
+      tab()
     }
   }
-};
+}
 
 exports.randomizeArray = function( array ) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length, temporaryValue, randomIndex
   while ( 0 !== currentIndex ) {
-    randomIndex = Math.floor( Math.random() * currentIndex );
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    randomIndex = Math.floor( Math.random() * currentIndex )
+    currentIndex -= 1
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
   }
-  return array;
+  return array
 }
 
 exports.leftPad = function( str, len, fillerChar=' ' ) {
   if( str.length > len ) { 
-    return;
+    return
   }
 
   if(fillerChar.length > 1 ) { 
-    fillerChar=fillerChar.substring(0,1);
+    fillerChar=fillerChar.substring(0,1)
   }
 
-  return fillerChar.repeat( len - str.length ) + str;
+  return fillerChar.repeat( len - str.length ) + str
+}
+
+exports.initCanvas = function() {
+  let canvasEl = document.createElement('canvas')
+  canvasEl.setAttribute('id', 'canvas')
+  document.body.appendChild( canvasEl )
 }
